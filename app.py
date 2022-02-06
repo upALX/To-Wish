6db089648908a4e8df526169990047938e2b1029
Alexandre <alexandreaddres@gmail.com>
2022-02-06 03:10:54 -0300

    Resolve the rendering bug and create the HTTP 304 bug to resolve

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ class Wishes(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.id
 
+#UPDATE wishes
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    wish_update = Wishes.query.get_or_404(id)
+    if request.method == "POST":
+        wish_update.name = request.form['name_update']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Ops, has a problem on update...'
+    else:
+        return render_template('update.html', wish_update=wish_update) 
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     titleIndex = 'To Wish'
@@ -34,19 +49,4 @@ def index():
     else:
         wishes = Wishes.query.order_by(Wishes.date_create)
         return render_template("index.html", title=titleIndex, wishes=wishes)
-
-#UPDATE wishes
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    titleUpdate = 'Update wish'
-    wish_update = Wishes.query.get_or_404(id)
-    if request.method == 'POST':
-        wish_update.item = request.form['wish-maked']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "Oh, this it is a problem with update,sorry... "
-        else: #bug - no render template
-            return render_template("index.html", wish_update=wish_update)
 
